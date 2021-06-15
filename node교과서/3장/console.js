21637d1ba4a2f3877fd7deb513f2090aadef907f
Inseo Hwang <sjsjsj1246@gmail.com>
2021-06-15 22:36:22 +0900

    ✨ console

---
+++ node교과서/3장/console.js
@@ -0,0 +1,37 @@
+const string = "abc";
+const number = 1;
+const boolean = true;
+const obj = {
+  outside: {
+    inside: {
+      key: "value",
+    },
+  },
+};
+
+console.time("전체시간");
+console.log("평범한 로그");
+console.log(string, number, boolean);
+console.error("에러 메세지");
+
+console.table([
+  { name: "제로", birth: 1994 },
+  { name: "hero", birth: 1988 },
+]);
+
+console.dir(obj, { colors: false, depth: 2 });
+console.dir(obj, { colors: true, depth: 1 });
+
+console.time("시간측정");
+for (let i = 0; i < 100000; i++) {}
+console.timeEnd("시간측정");
+
+function b() {
+  console.trace("시간 측정");
+}
+function a() {
+  b();
+}
+a();
+
+console.timeEnd("전체시간");
